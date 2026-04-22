3e0cf97d4f16caeff939b08d58370aacfd7bb374
PMarkovLumitech <pavel.markov@lumitech.software>
2026-04-22 14:07:54 +0300

    chore: init commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent-iad6-1.cdninstagram.com",
+        port: "",
+        pathname: "/**",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
